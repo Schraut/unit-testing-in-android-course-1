8c989b2a3db832542d298766b995abe5440e1666
Dan Shrout <danshrout1@gmail.com>
2021-03-09 10:03:45 -0800

    first unit test compete

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,29 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+    // SUT stands for system under test
+    NegativeNumberValidator SUT;
+
+    // Before annotation is used on a method containing java code to run before
+    // each test case. It runs before each test execution.
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+    // After annotation is used on a method containing java code to run after each
+    // test case
+
+    @Test
+    public void test1() {
+        boolean result = SUT.isNegative(-1);
+        Assert.assertThat(result, is(true));
+    }
 
 }
